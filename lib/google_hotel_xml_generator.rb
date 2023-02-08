8265e04ce6f2759d4ade8061312b58436b0654b0
hara kazuya <harakazuya@haranoMacBook-Pro.local>
2023-02-08 14:41:30 +0900

    first commit

---
+++ lib/google_hotel_xml_generator.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "google_hotel_xml_generator/version"
+
+module GoogleHotelXmlGenerator
+  class Error < StandardError; end
+  # Your code goes here...
+end
